5803d3af0ff9b9284df6af0611f6d2cd33904eef
SmirnovBoris <smirnofboris@gmail.com>
2014-04-14 14:38:06 +0400

    add problem

---
+++ PoliginBase/PoliginBase/Models/Problem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PoliginBase.Models
+{
+    public class Problem
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required]
+        public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Required]
+        public string Format { get; set; }
+        
+        [DataType(DataType.MultilineText)]
+        [Required]
+        public string Config { get; set; }
+
+    }
+}
