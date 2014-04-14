ebcb74ac8faa2f2a81b1653e2095c1d890aa6116
SmirnovBoris <smirnofboris@gmail.com>
2014-04-14 14:00:02 +0400

    Add ProblemTemplate and ProblemTemplateContext

---
+++ PoliginBase/PoliginBase/Models/ProblemTemplate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PoliginBase.Models
+{
+    public class ProblemTemplate
+    {
+        [DataType(DataType.Text)]
+        public string name { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string format { get; set; }
+        
+        [DataType(DataType.MultilineText)]
+        public string config { get; set; }
+
+    }
+}
---
+++ PoliginBase/PoliginBase/Models/ProblemTemplateContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PoliginBase.Models
+{
+    public class ProblemTemplateContext
+    {
+        public DbSet<ProblemTemplate> Problems { get; set; }
+    }
+}
